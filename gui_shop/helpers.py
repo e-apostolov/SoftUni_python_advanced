3d8450671e5fe6935dbad33a8c6c3f13950bd6d2
Evgeni Apostolov <133991275+e-apostolov@users.noreply.github.com>
2023-10-14 11:57:35 +0300

    Add files via upload

---
+++ gui_shop/helpers.py
@@ -0,0 +1,6 @@
+from gui_shop.canvas import app
+
+
+def clean_screen():
+    for el in app.grid_slaves():
+        el.destroy()
---
+++ gui_shop/main.py
@@ -0,0 +1,7 @@
+from gui_shop.canvas import app
+from gui_shop.authentication import render_main_enter_screen
+
+
+if __name__ == "__main__":
+    render_main_enter_screen()
+    app.mainloop()
